636fce0f8bc1a308d48fc29bfbdc8b8c39ff4805
rafzul <rafi.zulfahmi@mail.ugm.ac.id>
2022-12-17 13:21:41 +0700

    gitignore venv

--- extract_entsoe.py
+++ extract_entsoe.py
@@ -1,8 +1,13 @@
-import os 
+import os
 from dotenv import load_dotenv
 import requests
 from google.oauth2 import service_account
+import xmltodict
+import json
+from entsoe import EntsoeRawClient
+import pandas as pd
 
+# load env variables
 load_dotenv()
 
 security_token = os.environ.get("SECURITY_TOKEN")
@@ -12,10 +17,30 @@ credentials = service_account.Credentials.from_service_account_file(
 )
 
 headers = {
-    "Content-Type":"text/xml",
+    "Content-Type": "text/xml",
 }
 
-entsoe_url = f"https://transparency.entsoe.eu/api?securityToken={security_token}&documentType=A65&processType=A16&outBiddingZone_Domain=10YCZ-CEPS-----N&periodStart=201512312300&periodEnd=201612312300"
-entsoe_data = requests.get(entsoe_url, headers=headers)
+# entsoe_url = f"https://transparency.entsoe.eu/api?securityToken={security_token}&documentType=A73&processType=A16&in_Domain=10YCZ-CEPS-----N&periodStart=202212141200&periodEnd=202212141300"
+# entsoe_data = requests.get(entsoe_url, headers=headers)
+# entsoe_dict = xmltodict.parse(entsoe_data.text)
 
-print(entsoe_data)
+# using entsoe library
+client = EntsoeRawClient(api_key=security_token)
+start = pd.Timestamp("202112141200", tz="Europe/Brussels")
+end = pd.Timestamp("202112141300", tz="Europe/Brussels")
+country_code = "BE"  # Belgium
+country_code_from = "FR"  # France
+country_code_to = "DE_LU"  # Germany-Luxembourg
+type_marketagreement_type = "A01"
+contract_marketagreement_type = "A01"
+
+result = client.query_generation(country_code, start, end, psr_type=None)
+entsoe_dict = xmltodict.parse(result)
+
+
+with open("entsoe_data2.json", "w") as f:
+    json.dump(entsoe_dict, f, indent=4)
+
+print(entsoe_dict)
+
+# open file with context manager
