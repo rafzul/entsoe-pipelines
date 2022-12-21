e365dd70b47e6efefc16617ef94c0be46e3f0a67
rafzul <rafi.zulfahmi@mail.ugm.ac.id>
2022-12-21 12:53:28 +0700

    prototyping

--- extract_entsoe.py
+++ extract_entsoe.py
@@ -20,22 +20,23 @@ headers = {
     "Content-Type": "text/xml",
 }
 
-# entsoe_url = f"https://transparency.entsoe.eu/api?securityToken={security_token}&documentType=A73&processType=A16&in_Domain=10YCZ-CEPS-----N&periodStart=202212141200&periodEnd=202212141300"
-# entsoe_data = requests.get(entsoe_url, headers=headers)
-# entsoe_dict = xmltodict.parse(entsoe_data.text)
+entsoe_url = f"https://transparency.entsoe.eu/api?securityToken={security_token}&documentType=A73&processType=A16&in_Domain=10YCZ-CEPS-----N&periodStart=202212141200&periodEnd=202212141300"
+entsoe_data = requests.get(entsoe_url, headers=headers)
+entsoe_dict = xmltodict.parse(entsoe_data.text)
 
-# using entsoe library
-client = EntsoeRawClient(api_key=security_token)
-start = pd.Timestamp("202112141200", tz="Europe/Brussels")
-end = pd.Timestamp("202112141300", tz="Europe/Brussels")
-country_code = "BE"  # Belgium
-country_code_from = "FR"  # France
-country_code_to = "DE_LU"  # Germany-Luxembourg
-type_marketagreement_type = "A01"
-contract_marketagreement_type = "A01"
+# # using entsoe library
+# client = EntsoeRawClient(api_key=security_token)
+# start = pd.Timestamp("202101011200", tz="Europe/Berlin")
+# end = pd.Timestamp("202101011300", tz="Europe/Berlin")
+# country_code = "DE"  # Germany
+# country_code_from = "FR"  # France
+# country_code_to = "DE_LU"  # Germany-Luxembourg
+# type_marketagreement_type = "A01"
+# contract_marketagreement_type = "A01"
 
-result = client.query_generation(country_code, start, end, psr_type=None)
-entsoe_dict = xmltodict.parse(result)
+
+# result = client.query_generation(country_code, start, end, psr_type=None)
+# entsoe_dict = xmltodict.parse(result)
 
 
 with open("entsoe_data2.json", "w") as f:
